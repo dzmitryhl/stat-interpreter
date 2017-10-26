d05cb5b7bc3db5eb6e880ee3728171f5c086337a
DzmitryHlushchanka <dzmitry.hlushchanka@gmail.com>
2017-10-26 16:14:18 +0300

    Upgrade to Angular 4.2.4

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class StatInterpreterPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
